f1efb41d13135ee7c00282327fb57fe8575075c5
phette23 <phette23@gmail.com>
2015-02-16 11:12:52 -0800

    install drush, too

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 command -v brew >/dev/null || (echo 'Please install homebrew first (opening home page for you now…' && sleep 1 && open 'http://brew.sh' && exit)
-brew install fish chromedriver
+brew install chromedriver drush fish
 echo 'Need an admin password to install Selenium browser automation framework'
 sudo pip install selenium
