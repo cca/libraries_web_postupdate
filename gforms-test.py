f5cfe30956e131f002e664eea046900bb7b7e8b5
phette23 <phette23@gmail.com>
2015-09-08 10:02:07 -0700

    add (untested) instruction form test, refactor a bit

--- gforms-test.py
+++ gforms-test.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+from __future__ import print_function
 from selenium import webdriver
 from time import sleep
+from sub_process import call
 import webbrowser
 """
 test the libraries' Google Forms which are hooked up to Google Apps Script
@@ -11,7 +13,8 @@ send emails without warning, this checks that scripts still have permissions
 form_root = 'https://docs.google.com/a/cca.edu/forms/d/{0}/viewform'
 forms = {
     'acquisitions': '17pKoZ_U8AHbFjEg4blXiJod8uZwKI0s1DFJBSSSB7rE',
-    'ill': '1pyhf7krGP3PQX1McJIt1fwLUYAwUd88vGG1brC33qe8'
+    'ill': '1pyhf7krGP3PQX1McJIt1fwLUYAwUd88vGG1brC33qe8',
+    'instruction': '1i6Xn0q3MI7EXM6VpxUr64oywifGDcU-4NZoitp2y7-g'
 }
 # values submitted through form
 vals = {
@@ -64,6 +67,23 @@ def test_ill():
     browser.find_element_by_id('ss-submit').click()
 
 
+def test_instruction():
+    """
+    test the instruction session request form
+    """
+    browser.get(form_root.format(forms['instruction']))
+    # wait for user to manually sign into Google
+    # @TODO should automate this, probably by passing credentials on the cli
+    print('Waiting to sign into Google, hit Return when done...')
+    call('read')  # gotta be a native Python way to do this a/o/t shelling out
+    # course name
+    browser.find_element_by_id('entry_93795666').send_keys('TESTS-101')
+    # date/time, string sent looks weird here but it's just the date
+    # 01/01/200000 10:00 AM with the date/time autofill behavior
+    browser.find_element_by_id('entry_608498536').send_keys('0101200000100A')
+    browser.find_element_by_id('ss-submit').click()
+
+
 def open_sheets():
     """
     Open the back-end spreadsheets for each form
@@ -72,11 +92,11 @@ def open_sheets():
     sheet_root = 'https://docs.google.com/spreadsheets/d/'
     sheets = {
         'acquisitions': '1A0bBB4_mYWtcWoqfB5HP6H7WbIWjTknzHnVr-5EPw8s/edit#gid=1311627534',
-        'ill': '1V4yaJajQ37oGEFZXYxGcTtlF6Jq_XhQvZVGF4xBWO1k/edit#gid=275875544'
+        'ill': '1V4yaJajQ37oGEFZXYxGcTtlF6Jq_XhQvZVGF4xBWO1k/edit#gid=275875544',
+        'instruction': '1WpdyyvrVjCrWzHprUOZ1IlDmU2pQEb2qrzNML9G2JRw/edit#gid=1190654636'
     }
     for sheet in sheets:
-        webbrowser.open(sheet_root + sheet)
-        webbrowser.open(sheet_root + sheet)
+        webbrowser.open(sheet_root + sheets[sheet])
 
 
 if __name__ == '__main__':
@@ -85,9 +105,10 @@ if __name__ == '__main__':
     # browser = webdriver.PhantomJS()
     browser = webdriver.Chrome()
     browser.implicitly_wait(30)
-    test_recommendations()
-    sleep(1)
-    test_ill()
-    sleep(1)
+
+    for test in (test_recommendations, test_ill, test_instruction):
+        test()
+        sleep(1)
+
     open_sheets()
     browser.quit()
